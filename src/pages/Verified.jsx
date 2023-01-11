d9d8a4473511af7f4eed375473f41ae1ecd7ee0c
kelani34 <kelanitaiwo34@gmail.com>
2023-01-11 15:10:18 +0100

    update: finish verify page

--- src/pages/Verified.jsx
+++ src/pages/Verified.jsx
@@ -1,9 +1,80 @@
-import React from 'react'
-
+import React from "react";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+import VerifiedIcon from "../assets/verified.png";
+import rightArrow from "../assets/right-arrow.svg";
 const Verified = () => {
   return (
-    <div>Verified</div>
-  )
-}
+    <Wrapper>
+      <div className="container">
+        <div className="verified__card">
+          <img
+            src={VerifiedIcon}
+            alt="verified"
+            className="verified__card-img"
+          />
+          <p>Your email address has been verified.</p>
+          <Link to="/dashboard">
+            Go to Dashboard <img src={rightArrow} alt="right arrow" />
+          </Link>
+        </div>
+      </div>
+    </Wrapper>
+  );
+};
 
-export default Verified
+export default Verified;
+
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+  .container {
+    width: 720px;
+    height: 450px;
+    left: 360px;
+    top: 159px;
+    background: #ffffff;
+    border: 1px solid #f0f0f0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    .verified__card {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      .verified__card-img {
+        width: 64px;
+        height: 64px;
+        margin-bottom: 36px;
+      }
+      p {
+        margin-bottom: 21px;
+        font-family: "Montserrat";
+        font-style: normal;
+        font-weight: 500;
+        font-size: 20px;
+        line-height: 24px;
+
+        color: #000000;
+      }
+      a {
+        display: flex;
+        align-items: center;
+        gap: 10px;
+        text-decoration: none;
+        color: #004cbd;
+        font-family: "Montserrat";
+        font-style: normal;
+        font-weight: 500;
+        font-size: 16px;
+        line-height: 20px;
+        /* identical to box height */
+
+        color: #004cbd;
+      }
+    }
+  }
+`;
